8d2aeb855a97e2e6b09107bd2dcd326c876a1fd8
Adam Menz <mz.adam@gmail.com>
2014-04-07 16:45:46 -0400

    Working on the header

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
 		var currentXPos = $("#nav-bar").css('top');
 		var wholeNavBarHeight = $("#top-bar-whole").css('height');
 
-		if(currentXPos == '0px') {
-			$("#nav-bar").css('top', '-45px');
+		if(currentXPos == '-10px') {
+			$("#nav-bar").css('top', '-55px');
 			$("#top-bar-whole").css('height', '46px');
-			$("#wrap").css('top', '-45px');
+			$("#wrap").css('top', '-35px');
 		} else {
-			$("#nav-bar").css('top', '0px');
+			$("#nav-bar").css('top', '-10px');
 			$("#top-bar-whole").css('height', '91px');
 			$("#wrap").css('top', '0px');
 		}
